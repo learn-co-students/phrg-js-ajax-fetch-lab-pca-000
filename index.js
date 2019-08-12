7d95744737de2e47d8c8a14d64afc203ba8d4ccd
Gillian Wenhold <gwenhold@mac.com>
2019-08-12 14:56:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 function getIssues() {
+  fetch(`https://api.github.com/repos/gillianwenhold/javascript-fetch-lab/issues`, {
+    method: 'get',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showIssues(json));
 }
 
 function showIssues(json) {
+  const issues = json.map(i =>
+    `<h3>${i.title}</h3>
+    <p>${i.body}</p>`)
+  $("#issues").html(issues);
 }
 
 function createIssue() {
-}
-
-function showResults(json) {
+  console.log("In createIssue");
+  const title = document.getElementById('title').value
+  const body = document.getElementById('body').value
+  const data = { title: title, body: body }
+//  const link = document.getElementById("link").innerHTML.replace('https://github.com/', '');
+  fetch(`https://api.github.com/repos/gillianwenhold/javascript-fetch-lab/issues`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(data)
+  }).then(res => res.json()).then(getIssues());
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showForkedRepo(json.full_name));
 }
+
+function showForkedRepo(res) {
+  console.log('in showForkedRepo')
+  $('#results').append(`Forked! <a href="https://github.com/${res}" target="_blank" id="link">https://github.com/${res}</a>`)
+};
 
 function getToken() {
   //change to your token to run in browser, but set
